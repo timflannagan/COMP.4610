4b78a1826a96aa0ac124e08fa3b80203c931c295
timflannagan1 <timflannagan@gmail.com>
2018-11-15 22:17:23 -0500

    Update js/hw6.js to use row and column headings

--- hw6/js/hw6.js
+++ hw6/js/hw6.js
@@ -1,13 +1,18 @@
 function validate_input(row1, row2, col1, col2) {
-  if (row1.value > row2.value) {
-    row2.style.backgroundColor = "red";
+  if (row1.value === "" || row2.value === "") {
+    alert("Empty value(s) entered for row bounds.");
+    return false;
+  }
+
+  if (Number(row1.value) > Number(row2.value)) {
+    row2.style.backgroundColor = "rgb(218, 143, 180)";
     alert('Upper bound row value needs to be less than lower bound row value');
     return false;
   }
 
-  if (col1.value > col2.value) {
-    col2.style.backgroundColor = "red";
-    alert("Upper bound col value needs to be less than lower bound col value");
+  if (Number(col1.value) > Number(col2.value)) {
+    col2.style.backgroundColor = "rgb(218, 143, 180)";
+    alert("Upper bound col value needs to be less than lower bound col value; You entered: " + col1.value + " and " + col2.value);
     return false;
   }
 
@@ -26,22 +31,48 @@ function create_table() {
   var col_val = col1.value;
 
   if (!validate_input(row1, row2, col1, col2)) {
-    // clear form data if invalid
+    // To-Do: clear form data if invalid
     return;
   }
 
-  // iterate over the values entered in the form data: create row first then col data.
-  for (var i = 0; i < (row2.value - row1.value) + 1; i++) {
-    var header_cell = document.createElement('TH');
-    var curr_row = table.insertRow(i);
+  var first_row = table.insertRow(0);
 
-    for (var j = 0; j < (col2.value - col1.value) + 1; j++) {
+  for (var i = 0; i < 1; i++) {
+    for (var j = 0; j < (col2.value - col1.value) + 2; j++) {
+      var first_col = first_row.insertCell();
+
+      if (j === 0) {
+        first_col.innerHTML = "";
+      } else {
+        first_col.innerHTML = col_val;
+        col_val++;
+      }
+    }
+  }
+
+  col_val = col1.value;
+
+  // iterate over the values entered in the form data: create row first then col data.
+  for (var i = 1; i < (row2.value - row1.value) + 2; i++) {
+    var curr_row = table.insertRow(i);
+    // var row_header = document.createElement("TH");
+    //
+    // row_header.innerHTML = row_val;
+    // curr_row.appendChild(row_header);
+
+    for (var j = 0; j < (col2.value - col1.value) + 2; j++) {
       var curr_col = curr_row.insertCell(j);
 
-      curr_col.innerHTML = row_val * col_val;
-      col_val++;
+      if (j == 0)
+      {
+        curr_col.innerHTML = row_val;
+      } else {
+        curr_col.innerHTML = row_val * col_val;
+        col_val++;
+      }
     }
 
+    col_val = col1.value;
     row_val++;
   }
 
