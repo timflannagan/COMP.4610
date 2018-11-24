59887e1a91e0d35a6e885896f176f04ed4979674
timflannagan1 <timflannagan@gmail.com>
2018-11-24 17:04:23 -0500

    Handle lower and upper bound row/column input validation

--- hw7/js/form-validation.js
+++ hw7/js/form-validation.js
@@ -11,47 +11,39 @@ Sources:
 3. https://formden.com/blog/validate-contact-form-jquery
 4. https://stackoverflow.com/questions/15060292/a-simple-jquery-form-validation-script
 5. https://stackoverflow.com/questions/18041140/jquery-validate-plugin-submithandler-delaying-form-submit
+6. https://stackoverflow.com/questions/29451507/how-to-use-jquery-validator-to-determine-value-of-one-field-is-greater-than-anot
 */
 
-// $.validator.addMethod("upperbound_greater_than_lowerbound"), function(value, element) {
-//   return row1 > row2
-// }
-
-
-
 $(document).ready(function () {
-    $.validator.addMethod("row2_greater_row1", function(value, element) {
-        return row2 > row1
+    $.validator.addMethod("upperbound_greater_lowerbound", function (value, element, param) {
+              var $otherElement = $(param);
+              return parseInt(value, 10) > parseInt($otherElement.val(), 10);
     });
 
-    $.validator.addMethod("col2_greater_col1", function(value, element) {
-        return col2 > col1
-    });
-
-    $('#myform').validate({
+    var validator = $('#myform').validate({
         rules: {
           /* Ensure the form input values are correctly inputted */
           row1: {
               required: true,
               digits: true,
-              range: [1, 100]
+              range: [0, 100]
           },
           row2: {
               required: true,
               digits: true,
               range: [Number(row1.value) + 1, 100],
-              row2_greater_row1: true
+              upperbound_greater_lowerbound: "#row1"
           },
           col1: {
               required: true,
               digits: true,
-              range: [1, 100]
+              range: [0, 100]
           },
           col2: {
               required: true,
               digits: true,
-              range: [1, 100],
-              col2_greater_col1: true
+              range: [Number(col1.value) + 1, 100],
+              upperbound_greater_lowerbound: "#col1"
           }
         },
         /* Output a relevant message based on the type of incorrect field entry. */
@@ -63,7 +55,7 @@ $(document).ready(function () {
           row2: {
             required: "This is a required field!",
             digits: "Enter an integer for the upper bound row value.",
-            row2_greater_row1: "You need to enter a value greater than row1."
+            upperbound_greater_lowerbound: "You need to enter a value greater than row1."
 
           },
           col1: {
@@ -73,8 +65,13 @@ $(document).ready(function () {
           col2: {
             required: "This is a required field!",
             digits: "Enter an integer for the upper bound column value.",
-            col2_greater_col1: "You need to enter a value greater than col1."
+            upperbound_greater_lowerbound: "You need to enter a value greater than col1."
           }
+        },
+        submitHandler: function (form) {
+            form.submit();
         }
     });
+    
+    validator.form();
 });
