36546d4c578457aa7daf347051a13c7b49235fee
timflannagan1 <timflannagan@gmail.com>
2018-12-02 17:20:14 -0500

    Add text to the slider handler.

--- hw8/js/create_sliders.js
+++ hw8/js/create_sliders.js
@@ -1,3 +1,13 @@
+/*
+Name: Tim Flannagan
+Email: timothy_flannagan@student.uml.edu
+Assignment #8
+Date: 12/02
+
+Sources:
+1.
+*/
+
 function create_sliders() {
     $('#first-row').slider({
         min: 1,
@@ -5,9 +15,19 @@ function create_sliders() {
         range: [1, 30],
         value: 5,
 
-        slide: function( event, ui ) {
+        slide: function(event, ui) {
             $("#row1").val(ui.value);
+            $(this).find('.ui-slider-handle').text(ui.value);
+        },
+        create: function(event, ui) {
+            var v = $(this).slider('value');
+            $(this).find('.ui-slider-handle').text(v);
         }
+    });
+
+    $("#row1").change(function () {
+        console.log('row1 was changed. updating in row1 slider.');
+        $("#first-row").slider("option", "value");
     });
 
     $('#second-row').slider({
@@ -16,8 +36,12 @@ function create_sliders() {
         range: [1, 30],
         value: 5,
 
-        slide: function( event, ui ) {
+        slide: function(event, ui) {
             $("#row2").val(ui.value);
+        },
+        create: function(event, ui) {
+            var v = $(this).slider('value');
+            $(this).find('.ui-slider-handle').text(v);
         }
     });
 
@@ -29,6 +53,10 @@ function create_sliders() {
 
         slide: function( event, ui ) {
             $("#col1").val(ui.value);
+        },
+        create: function(event, ui) {
+            var v = $(this).slider('value');
+            $(this).find('.ui-slider-handle').text(v);
         }
     });
 
@@ -40,6 +68,22 @@ function create_sliders() {
 
         slide: function( event, ui ) {
             $("#col2").val(ui.value);
+        },
+        create: function(event, ui) {
+            var v = $(this).slider('value');
+            $(this).find('.ui-slider-handle').text(v);
         }
     });
+
+
+    // var row1_value = $("first-row").slider("option", "value");
+    // var row2_value = $("second-row").slider("option", "value");
+    // var col1_value = $("first-col").slider("option", "value");
+    // var col2_value = $("second-col").slider("option", "value");
+    //
+    // $("row1").val(row1_value);
+    // $("row2").val(row2_value);
+    // $("col1").val(row2_value);
+    // $("col2").val(row2_value);
+
 };
