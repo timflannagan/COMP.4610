5212a865b170d83e2d1d799d6745ed23fbfaaa7d
Tim Flannagan <timflannagan@gmail.com>
2018-12-11 15:36:39 -0500

    Update data-html.js

--- inclass/inclass8_handout/js/data-html.js
+++ inclass/inclass8_handout/js/data-html.js
@@ -2,14 +2,14 @@
 // You will not get a server status and the example will fail
 // Comment out lines 9 and 11 if you are working locally
 
-var xhr = new XMLHttpRequest();                 // comment here
+var xhr = new XMLHttpRequest();                 // Constructor call to initialize an XMLHttpRequest Object
 
-xhr.onload = function() {                       // comment here
+xhr.onload = function() {                       // Wait till response is loaded
   // The following conditional check will not work locally - only on a server
-  if(xhr.status === 200) {                       // comment here
-    document.getElementById('content').innerHTML = xhr.responseText; // comment here
+  if(xhr.status === 200) {                       // Check whether the request was successfully received/loaded/accepted
+    document.getElementById('content').innerHTML = xhr.responseText; // Update the #content element's html with the xhr response
   }
 };
 
-xhr.open('GET', 'data/data.html', true);        // comment here
-xhr.send(null);                                 // comment here
+xhr.open('GET', 'data/data.html', true);        // Initialize a newly-created request with get method
+xhr.send(null);                                 // Send the request to the server
